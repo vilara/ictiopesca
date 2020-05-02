7ce8d89f10329be4c9e552848f8c414fd391430b
Marcelo Vilara <marcelovilara@gmail.com>
2020-05-02 15:05:48 -0300

    inicio4

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,9 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    
+                    <div><img alt="" src="/svg/peixe.svg" style="height: 28px"></div>
+                    <div>Ictiopesca</div>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
