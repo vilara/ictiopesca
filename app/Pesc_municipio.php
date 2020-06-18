98086207ca140f8a49204cdbcda0c3ecc13328e5
Marcelo Vilara <marcelovilara@gmail.com>
2020-06-17 21:08:11 -0300

    AWS_object3_localidades

--- app/Pesc_municipio.php
+++ app/Pesc_municipio.php
@@ -6,8 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Pesc_municipio extends Model
 {
-    public function pesc_localidades()
+    public function pesc_localidade()
     {
-        return $this->hasMany('App\Pesc_localidade');
+        return $this->hasMany('App\Pesc_localidade','municipio_id','id');
     }
 }
