fa8b4cf33d3d673f431b8a9221abd946933a1d02
Marcelo Vilara <marcelovilara@gmail.com>
2020-07-05 11:14:22 -0300

    mercado_corr

--- app/Http/Controllers/PescMercadoController.php
+++ app/Http/Controllers/PescMercadoController.php
@@ -22,12 +22,13 @@ class PescMercadoController extends Controller
             
 
             $mercado = DB::table('pesc_col_mercado')
-            ->join('pesc_especies', 'pesc_col_mercado.cat', '=', 'pesc_especies.id')
-            ->join('pesc_localidades', 'pesc_col_mercado.loc', '=', 'pesc_localidades.id')
-            ->select('pesc_col_mercado.id','pesc_especies.cat','pesc_localidades.localidade',
-                    'pesc_col_mercado.ct','pesc_col_mercado.cp','pesc_col_mercado.pt',
-                    'pesc_col_mercado.cab','pesc_col_mercado.sexo','pesc_col_mercado.cresc',
-                'pesc_col_mercado.gg','pesc_col_mercado.gr','pesc_col_mercado.data'
+//             ->join('pesc_especies', 'pesc_col_mercado.cat', '=', 'pesc_especies.id')
+//             ->join('pesc_localidades', 'pesc_col_mercado.loc', '=', 'pesc_localidades.id')
+            ->select('pesc_col_mercado.id',
+//                 'pesc_especies.cat','pesc_localidades.localidade',
+//                     'pesc_col_mercado.ct','pesc_col_mercado.cp','pesc_col_mercado.pt',
+//                     'pesc_col_mercado.cab','pesc_col_mercado.sexo','pesc_col_mercado.cresc',
+//                 'pesc_col_mercado.gg','pesc_col_mercado.gr','pesc_col_mercado.data'
 //                 'pesc_col_mercado.cf',
 //                 'pesc_col_mercado.gen','pesc_col_mercado.od','pesc_col_mercado.ap',
                 );
